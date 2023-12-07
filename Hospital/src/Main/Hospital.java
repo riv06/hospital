86fffef951ac44699bda8a0c78ae92b4787a7184
TastyVI <mister20124@gmail.com>
2023-12-07 15:39:45 -0600

    Ejecutar desde Main.Hospital

---
+++ Hospital/src/Main/Hospital.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Formularios.Inicio;
+
+public class Hospital {
+
+    public static void main(String[] args) {
+        Inicio principal = new Inicio();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+    
+}
